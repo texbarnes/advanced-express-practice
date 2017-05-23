07402b3758187a6b4f82f6ea92faa55ce11b9b21
Jon Woo <jw56578@gmail.com>
2017-05-23 15:18:36 -0500

    readme

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -9,14 +9,14 @@ function Contacts() {
   return (
     <div>
       <div style={{float: "left", width: "49%"}}>
-        <h1>Contacts</h1>
-        <ContactsContainer />
-        <h1>Products</h1>
-        <ProductsContainer />
-        <h1>Vehicles</h1>
-        <VehiclesContainer />
         <h1>Comments </h1>
         <CommentsContainer />
+        <h1>Contacts</h1>
+        <ContactsContainer />
+        <h1>Vehicles</h1>
+        <VehiclesContainer />
+        <h1>Products</h1>
+        <ProductsContainer />
       </div>
       <div style={{float: "left", width: "49%"}}>
         <CreateThingsContainer />
