cfa74d38b07c24bef5453941778bae8995b2cd0c
Jon Woo <jw56578@gmail.com>
2018-04-23 15:38:10 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,3 @@ let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
-const app = express();
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Listening on port:${port}`);
-});
